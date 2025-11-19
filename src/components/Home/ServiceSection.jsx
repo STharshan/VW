ebb4b93a0847d1b10a8d8d05fbae7e37ee720f53
STharshan <sivatharshan81@gmail.com>
2025-11-20 00:13:05 +0530

    a

--- src/components/Home/ServiceSection.jsx
+++ src/components/Home/ServiceSection.jsx
@@ -151,7 +151,7 @@ const ServiceSection = () => {
                   <img
                     src={service.image}
                     alt={service.title}
-                    className="absolute inset-0 w-[75%] mt-25 mx-auto h-15 object-cover"
+                    className="absolute inset-0 w-[75%] mt-25 mx-auto h-45 object-cover"
                     onError={(e) => {
                       e.currentTarget.onerror = null;
                       e.currentTarget.src = "/logo.png"; // fallback logo
